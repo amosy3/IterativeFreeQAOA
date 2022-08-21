1c89ef97722db7ff2b902573933283767e6eb995
tamuzd <55927400+tamuzd@users.noreply.github.com>
2022-08-21 16:33:14 +0300

    train the network

--- train_network.py
+++ train_network.py
@@ -6,14 +6,15 @@ import numpy as np
 from torch.utils.data import TensorDataset, DataLoader
 import argparse
 
-parser = argparse.ArgumentParser(description='Generate all posible maxcut configuration and return their optimized parameters')
+parser = argparse.ArgumentParser(
+    description='Generate all posible maxcut configuration and return their optimized parameters')
 
-parser.add_argument("--number_of_nodes", type=int)
-parser.add_argument("--train_csv", type=str)
-parser.add_argument("--test_csv", type=str)
-parser.add_argument("--dataset_size", type=int, default=5000)
-
+parser.add_argument("-n", "--number_of_nodes", type=int)
+parser.add_argument("-p", "--layers", type=int)
+parser.add_argument("--csv_path", type=str)
 args = parser.parse_args()
+
+
 
 class Net(nn.Module):
 
@@ -21,7 +22,7 @@ class Net(nn.Module):
         super(Net, self).__init__()
         self.fc1 = nn.Linear(bit_len, 100)
         self.fc2 = nn.Linear(100, 100)
-        self.fc3 = nn.Linear(100, 2*p)
+        self.fc3 = nn.Linear(100, 2 * p)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
@@ -29,55 +30,50 @@ class Net(nn.Module):
         x = self.fc3(x)
         return x
 
-def list_of_binaries_from_decimal(num,Length_list):
-    """take a number and return list of its binary in a list.
-        add zeros to reach similar length
-    """
-    num_string = bin(num)[2:]
-    if len(num_string)>=Length_list:
-        return [int(char) for char in num_string]
-    else:
-        Add_Zeros_Num = Length_list - len(num_string)
-        num_string = Add_Zeros_Num*'0' + num_string
-        return [int(char) for char in num_string]
+
+
 
 def df_to_dataloader(df, p, n, batch_size=32, train=True):
-    # print(df.head())
-    edges = ['x%s' % i for i in range(n)]
+    edges = ['x(%s,%s)'%(i, j) for i in range(n) for j in range(1,n) if j>i]
     X = torch.tensor(df[edges].values)
-    # X = torch.tensor([list_of_binaries_from_decimal(int(z), n) for z in X])
+
     if train:
-        params = ['b%s'%i for i in range(p)] + ['g%s'%i for i in range(p)]
+        params = ['beta%s' % i for i in range(p)] + ['gamma%s' % i for i in range(p)]
         y = torch.tensor(df[params].values)
     else:
         y = torch.zeros(X.shape[0])
+
     dataset = TensorDataset(X, y)
     dataloader = DataLoader(dataset, batch_size=batch_size, pin_memory=True)
     return dataloader
 
+
 epochs = 100
-bit_len = int(args.number_of_nodes*(args.number_of_nodes-1)/2)
-train_df = pd.read_csv("train_datasets/"+args.train_csv+".csv", nrows=args.dataset_size)
-p = int((train_df.shape[1]-2-bit_len)/2)
-test_df = pd.read_csv("test_datasets/"+args.test_csv+".csv")
+bit_len = int(args.number_of_nodes * (args.number_of_nodes - 1) / 2)
+p = args.layers
 
-# msk = np.random.rand(len(df)) < 0.8
-# train_df = df[msk]
-# test_df = df[~msk]
+data_df = pd.read_csv("train_datasets/" + args.csv_path + ".csv")
+test_df = pd.read_csv("test_datasets/" + args.csv_path + "_test_data.csv")
 
-train_loader, test_loader = df_to_dataloader(train_df, p, bit_len), df_to_dataloader(test_df, p, bit_len, train=False)
-print('train size: %d. Test size: %d' %(len(train_df),len(test_df)))
+msk = np.random.rand(len(data_df)) < 0.8
+train_df = data_df[msk]
+validation_df = data_df[~msk]
+
+
+train_loader, validation_loader, test_loader = df_to_dataloader(train_df, p, args.number_of_nodes), \
+                            df_to_dataloader(validation_df, p, args.number_of_nodes), \
+                            df_to_dataloader(test_df, p, args.number_of_nodes, train=False)
+print('train size: %d. Validation size: %d. Test size: %d' % (len(train_df), len(validation_df) ,len(test_df)))
 
 net = Net(bit_len, p)
 criterion = nn.MSELoss()
 optimaizer = torch.optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
-
 for epoch in range(epochs):
     net.train()
     running_loss = 0
-    n=0
-    for X,y in train_loader:
+    n = 0
+    for X, y in train_loader:
         optimaizer.zero_grad()
         output = net(X.float())
         loss = criterion(output, y.float())
@@ -85,18 +81,17 @@ for epoch in range(epochs):
         optimaizer.step()
         n += X.shape[0]
         running_loss += loss.item()
-    
-    # net.eval()
-    # n_val = 0
-    # running_val_loss = 0
-    # with torch.no_grad():
-    #     for X,y in test_loader:
-    #         output = net(X.float())
-    #         loss = criterion(output, y.float())
-    #         n_val += X.shape[0]
-    #         running_val_loss += loss.item()
-    # print("Epoch: %d loss: %0.4f, val_loss:%0.4f" %(epoch, running_loss/n, running_val_loss/n_val))
-    print("Epoch: %d loss: %0.4f" % (epoch, running_loss / n))
+
+    net.eval()
+    n_val = 0
+    running_val_loss = 0
+    with torch.no_grad():
+        for X,y in validation_loader:
+            output = net(X.float())
+            loss = criterion(output, y.float())
+            n_val += X.shape[0]
+            running_val_loss += loss.item()
+    print("Epoch: %d loss: %0.4f, val_loss:%0.4f" %(epoch, running_loss/n, running_val_loss/n_val))
 
 net.eval()
 graphs, preds = [], []
@@ -109,8 +104,8 @@ graphs = np.concatenate(graphs)
 graphs_df = pd.DataFrame(graphs)
 
 preds = np.concatenate(preds)
-params = ['b%s'%i for i in range(p)] + ['g%s'%i for i in range(p)]
+params = ['beta%s' % i for i in range(p)] + ['gamma%s' % i for i in range(p)]
 preds_df = pd.DataFrame(preds, columns=params)
 
 result = pd.concat([graphs_df, preds_df], axis=1, join="inner")
-result.to_csv("preds/train_%s_test_%s_preds_dataset_size_%s.csv" %(args.train_csv, args.test_csv, args.dataset_size), index=False)
+result.to_csv("preds/" + args.csv_path + "_preds.csv", index=False)
